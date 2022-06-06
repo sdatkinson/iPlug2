a71b0f0b64ee97215ba4b6bdf42ddb361f933492
Steven Atkinson <steven@atkinson.mn>
2022-06-05 21:09:19 -0400

    Input and output level knobs (#2)

--- Examples/NAM/IPlugEffect.h
+++ Examples/NAM/IPlugEffect.h
@@ -2,9 +2,12 @@
 
 //#define TORCHIN
 
+#include <string>
+#include <vector>
+#include <unordered_map>
+
 #if IPLUG_DSP
 #include <memory>
-#include <vector>
 #include "dsp.h"
 #endif
 
@@ -14,7 +17,8 @@ const int kNumPresets = 1;
 
 enum EParams
 {
-  kGain = 0,
+  kInputGain = 0,
+  kOutputGain,
   kNumParams
 };
 
@@ -25,12 +29,17 @@ class NeuralAmpModeler final : public Plugin
 {
 public:
   NeuralAmpModeler(const InstanceInfo& info);
+  std::vector<std::string> _param_names;
 
 #if IPLUG_DSP // http://bit.ly/2S64BDd
   void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
 private:
   std::unique_ptr<DSP> dsp;
 
+  //Collect all of the parameters from the display to provide them to to the DSP module
+  std::unordered_map<std::string, double> _get_params();
+  // If something is wrong, then this implements a fallback so that we still ensure the
+  // Required output
   void ProcessFallback(sample** inputs, sample** outputs, const int nChans, const int nFrames);
 #endif
 };
