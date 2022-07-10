f6799f6f4ee4aadd2c95c117435140d3dfdbab1c
Steven Atkinson <steven@atkinson.mn>
2022-07-10 08:13:37 -0700

    Conditional LSTM model

--- Examples/NAM/IPlugEffect.h
+++ Examples/NAM/IPlugEffect.h
@@ -1,7 +1,5 @@
 #pragma once
 
-//#define TORCHIN
-
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -19,6 +17,13 @@ enum EParams
 {
   kInputGain = 0,
   kOutputGain,
+  kParametricGain,
+  kparametricLow,
+  kParametricMid,
+  kparametricHigh,
+  kParametricMaster,
+  kparametricPresence,
+  kParametricREsonance,
   kNumParams
 };
 
@@ -29,17 +34,19 @@ class NeuralAmpModeler final : public Plugin
 {
 public:
   NeuralAmpModeler(const InstanceInfo& info);
-  std::vector<std::string> _param_names;
 
 #if IPLUG_DSP // http://bit.ly/2S64BDd
   void ProcessBlock(sample** inputs, sample** outputs, int nFrames) override;
 private:
   std::unique_ptr<DSP> dsp;
 
-  //Collect all of the parameters from the display to provide them to to the DSP module
+  // Collect all of the parameters from the display to provide them to to the DSP module
   std::unordered_map<std::string, double> _get_params();
   // If something is wrong, then this implements a fallback so that we still ensure the
   // Required output
   void ProcessFallback(sample** inputs, sample** outputs, const int nChans, const int nFrames);
+
+  // Nolly 3
+  const std::vector<std::string> _param_names{ "Input", "Output", "Gain", "Low", "Mid", "High", "Master", "Presence", "Resonance" };
 #endif
 };
